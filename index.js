78a2a7f04f53cefd115a813c5fe11928c8d9c596
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2024-05-09 11:38:18 +0700

    Dynamic Object Type
    Type can be changed without declaration in js

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ console.log("Hello World");
 let interestRate = 0.3;
 interestRate = 1;
 console.log(interestRate);
+
+let person = {
+  name: "Mosh",
+  age: 30,
+};
+
+person.name = 15;
+
+console.log(typeof person.name);
